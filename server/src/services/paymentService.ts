8fa3487b2256d6ef676a380e5911b4c4b6af887f
usman-174 <hellmughal123@gmail.com>
2025-02-28 22:36:48 +0500

    added Stipe payments,admin payment view,user view payment history

--- server/src/services/paymentService.ts
+++ server/src/services/paymentService.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 const prisma = new PrismaClient();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-01-27.acacia",
+  apiVersion: "2025-02-24.acacia",
   typescript: true,
 });
 export const getPaymentsByUserIdPaginated = async (
